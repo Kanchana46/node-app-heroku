77a83491a2ae4bb6ae9c903e1b73fcefeaa45beb
Kanchana46 <kanchana.rdg@gmail.com>
2022-02-10 22:59:42 +0530

    changed

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 
 const app = express()
-
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello from Node.js!')
 })
 
-app.listen(3000, () => {
-    console.log('listening!')
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`)
 })
